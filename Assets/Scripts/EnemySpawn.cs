d4746994588c548bfce2f7e05dbde55539980624
Artem Uvarov <uvarov.artem16@yandex.ru>
2018-11-15 12:10:41 +0300

    move enemy

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public int WaveSize;
+
+    public GameObject EnemyPrefab;
+
+    public float EnemyInterval;
+
+    public Transform SpawnPoint;
+
+    public float StartTime;
+
+    public Transform[] WayPoints;
+
+    private int _enemyCount = 0;
+
+
+	void Start ()
+	{
+	    InvokeRepeating("SpawnEnemy", StartTime, EnemyInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (_enemyCount == WaveSize)
+	    {
+	        CancelInvoke("SpawnEnemy");
+	    }
+	}
+
+    void SpawnEnemy()
+    {
+        _enemyCount++;
+        var enemy = Instantiate(EnemyPrefab, SpawnPoint.position, Quaternion.identity);
+        enemy.GetComponent<MoveEnemy>().WayPoints = WayPoints;
+    }
+}
---
+++ Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public float Speed;
+
+    public Transform[] WayPoints;
+
+    private int curWayPointIndex = 0;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (curWayPointIndex > WayPoints.Length) return;
+	    transform.position =
+	        Vector3.MoveTowards(transform.position, WayPoints[curWayPointIndex].position, Time.deltaTime * Speed);
+	    if (Vector3.Distance(transform.position, WayPoints[curWayPointIndex].position) < 0.5f)
+	    {
+	        curWayPointIndex++;
+	    }
+
+	}
+}
